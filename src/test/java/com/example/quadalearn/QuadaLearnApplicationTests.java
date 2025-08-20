2e3cc965adc99fd90016586b359d387564a7c11b
Khuc Nhan <khucvii@gmail.com>
2025-08-20 15:51:43 +0700

    first commit

---
+++ src/test/java/com/example/quadalearn/QuadaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quadalearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadaLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
