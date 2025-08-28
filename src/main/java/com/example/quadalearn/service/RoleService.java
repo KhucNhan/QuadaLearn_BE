95eddd9a4d21dd3a8d65047b1e255abe9573ddb9
Khuc Nhan <khucvii@gmail.com>
2025-08-28 13:45:14 +0700

    base

---
+++ src/main/java/com/example/quadalearn/service/RoleService.java
@@ -0,0 +1,19 @@
+package com.example.quadalearn.service;
+
+import com.example.quadalearn.model.Role;
+import com.example.quadalearn.repository.IRoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoleService {
+    @Autowired
+    private IRoleRepository roleRepository;
+
+    public List<Role> findAll() {
+        return roleRepository.findAll();
+    }
+
+}
