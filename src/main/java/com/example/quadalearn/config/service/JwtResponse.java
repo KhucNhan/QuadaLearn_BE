1b4a60a43fb0c542e7ce0de4b2daa4e4531629e0
sheep <samcoxe0307@gmail.com>
2025-10-30 09:19:30 +0700

    fix profile header user avatar

--- src/main/java/com/example/quadalearn/config/service/JwtResponse.java
+++ src/main/java/com/example/quadalearn/config/service/JwtResponse.java
@@ -13,6 +13,7 @@ public class JwtResponse {
     private String token;
     private String type = "Bearer";
     private String name;
+    private String avatar;
     private boolean needsCompletion;
 
     private final Collection<? extends GrantedAuthority> authorities;
@@ -25,45 +26,20 @@ public class JwtResponse {
         this.authorities = authorities;
     }
 
-    public JwtResponse(Long id, String token, String name, Collection<? extends GrantedAuthority> authorities, boolean needsCompletion) {
+    public JwtResponse(Long id, String token, String name, String avatar, Collection<? extends GrantedAuthority> authorities, boolean needsCompletion) {
         this.id = id;
         this.token = token;
         this.name = name;
+        this.avatar = avatar;
         this.authorities = authorities;
         this.needsCompletion = needsCompletion;
     }
 
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
     }
 
     public boolean isNeedsCompletion() {
@@ -74,7 +50,43 @@ public class JwtResponse {
         this.needsCompletion = needsCompletion;
     }
 
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
